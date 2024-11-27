c2d6ead1914376e6278e9086a5e3a746a1947b43
Dario Jularic <dariojularic0@gmail.com>
2024-11-27 19:30:46 +0100

    Refactor code

--- src/PersonalInformation.jsx
+++ src/PersonalInformation.jsx
@@ -2,20 +2,20 @@ import PropTypes from "prop-types";
 import Input from "./Input";
 
 function PersonalInformation(props) {
-  const {personalInfo} = props
-  console.log(personalInfo)
+  const { personalInfo } = props;
+  console.log(personalInfo);
 
   return (
     <div className="personal-info">
+      {/* <Input />
       <Input />
       <Input />
       <Input />
       <Input />
       <Input />
-      <Input />
-      <Input />
+      <Input /> */}
     </div>
-  )
+  );
 }
 
-export default PersonalInformation
+export default PersonalInformation;
