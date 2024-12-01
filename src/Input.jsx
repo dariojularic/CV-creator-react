6e282d7fd1d3f47825e7a07db5847fa6554b1168
Dario Jularic <dariojularic0@gmail.com>
2024-12-01 22:19:23 +0100

    Refactor code

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,9 +1,9 @@
 import "./Input.css";
 import PropTypes from "prop-types";
 
-// type, id, name, required, minlegth, maxlength, size, placeholder, autocapitalize
 function Input(props) {
   const { id, type, placeholder, name, handleChange } = props;
+
   if (id === undefined) {
     return (
       <input
@@ -15,38 +15,21 @@ function Input(props) {
       />
     );
   } else {
-    // console.log(func)
     return (
       <input
         required={type === "date"}
         type={type}
         placeholder={placeholder}
         name={name}
-        // value={value}
         onChange={(event) => handleChange(id, event)}
-        // id={eduId}
       />
     );
   }
-
-  // console.log(props)
-  // console.log("id", id);
-  // if (id === undefined) console.log("jeeeeeeeeeeeje")
-  return (
-    <input
-      required={type === "date"}
-      type={type}
-      placeholder={placeholder}
-      name={name}
-      // value={value}
-      onChange={(event) => handleChange(event)}
-      // id={eduId}
-    />
-  );
 }
 
 Input.propTypes = {
   type: PropTypes.string,
+  id: PropTypes.string,
   placeholder: PropTypes.string,
   name: PropTypes.string,
   handleChange: PropTypes.func,
